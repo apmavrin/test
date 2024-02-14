d0f466a74f19982266f1529c96d640f10f4752f8
Anton Mavrin <mavrin@outlook.com>
2024-02-13 22:51:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function prepareWebAssistant() {
         // check requirements loaded - postpone if not
-        if (document.getElementsByTagName('head')[0] || document.body) {
+        if (!document.getElementsByTagName('head')[0] || !document.body) {
             window.setTimeout(()=>{console.log("hello")}, 5000);
             return;
         }})()  
